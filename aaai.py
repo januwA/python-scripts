93a7fec5d4576d0abc511f85a327e9d3824f193c
januwA <ajanuw1995@gmail.com>
2025-11-05 22:10:53 +0800

    1

--- aaai.py
+++ aaai.py
@@ -9,9 +9,9 @@ branch = subprocess.run(
 
 target = "refactor-nightly"
 commands = [
-    fr'''cat C:\Users\16418\.claude\commands\aja\ar.md | gemini -m gemini-2.5-flash --yolo''',
-    fr'''codex exec "读取 C:\Users\16418\.claude\commands\aja\ar.md 文件里的内容并执行任务" -m="gpt-5-code" --dangerously-bypass-approvals-and-sandbox''',
-    fr'''cat C:\Users\16418\.claude\commands\aja\ar.md | claude --permission-mode bypassPermissions -p''',
+    fr'''cat C:\Users\16418\.claude\commands\aja\ar3.md | gemini -m gemini-2.5-flash --yolo''',
+    fr'''codex exec "读取 C:\Users\16418\.claude\commands\aja\ar3.md 文件里的内容并执行任务" -m="gpt-5-codex" --dangerously-bypass-approvals-and-sandbox''',
+    fr'''cat C:\Users\16418\.claude\commands\aja\ar3.md | claude --permission-mode bypassPermissions -p''',
 ]
 
 if branch == target:
